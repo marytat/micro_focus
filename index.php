4bca4b2fbdf495020619d90611fff5268a17d2a4
marytat <36045284+marytat@users.noreply.github.com>
2018-02-01 19:07:17 +0300

    add files

---
+++ index.php
@@ -0,0 +1,16 @@
+<?
+require_once($_SERVER['DOCUMENT_ROOT']."/inc.php");
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>HPE Software / Micro Focus</title>
+	<link rel="stylesheet" type="text/css" href="style.css?v=11">
+    <meta name="viewport" content="width=1000px"/>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	<script type="text/javascript" src="/event/js/counter.js"></script>
+</head>
+<body>
+	<?php include 'data.inc';?>
+</body>
+</html>
